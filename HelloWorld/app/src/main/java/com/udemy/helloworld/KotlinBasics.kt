9909ab1e4c1de73616e96171231d5046c17b3205
chocholwin-dev <chocholwin.job@gmail.com>
2021-03-03 20:46:15 +0630

    update kotlin fundamental codes

--- HelloWorld/app/src/main/java/com/udemy/helloworld/KotlinBasics.kt
+++ HelloWorld/app/src/main/java/com/udemy/helloworld/KotlinBasics.kt
@@ -2,50 +2,87 @@ package com.udemy.helloworld
 
 fun main(){
     // TODO: Add new functionality
-    //immutable variable
-    val myName : String = "Kyaw Zin"
-    //mutable variable
-    var myName1 : String = "Cho Lwin"
-    val long : Long = 39_812_309_487_120_300
-    val intValue : Short = 23_12
+    println("Hello, world!!!")
+    // This is a comment. Comments start with //
+    // val is used for variables which are immutable (not changable)
+    val myName = "Denis"
+    // myName = "Michael" // ERROR: Val cannot be reassigned
 
-    val myStr : String = "Hello World!"
-    val myLastChar : Char = myStr[myStr.length - 1]
-    val myFirstChar : Char = myStr[0]
-    println("My First Character "+myFirstChar)
-    println("My Last Character "+myLastChar)
-    myName1 = "Cho Cho"
-    println("Immutable variable : "+myName)
-    println("Mutable variable : "+myName1)
+    /* This is a multi line comment starts with /* and ends with *'/ without the '
+     var is used for variables which are mutable/changeable.
+     kotlin is a strongly typed language that supports Type Inference.
+     The compiler will directly assume the size and type to a variable by that.
+     For example, if you assign any whole number to a variable,
+     the compiler will directly assume that the variable is of type Int
+    */ */
+    var myAge = 31
 
-    // Arithmetic operator (+, -, %, /, *)
-    var result = 5 + 3
-    val a = 5
-    val b = 3
-    val c = 4.0
-    result /= 2
-    var resultDouble : Double
-    resultDouble = a / c
-    println("Result $result")
-    println("Result Double $resultDouble")
+    // Integer TYPES: Byte (8 bit), Short (16 bit), Int (32 bit), Long (64 bit)
+    val myByte: Byte = 13
+    val myShort: Short = 125
+    val myInt: Int = 123123123
+    val myLong: Long = 12_039_812_309_487_120
 
-    // Comparison Operator (==, !=, <=, >=)
-    val isEqual = 3!=2
-    println("isEqual is $isEqual")
-    println("isLowerEqual3 ${-4<=3}")
-    println("is5isGreaterEqual5 ${5>=5}")
+    // Floating Point number Types: Float (32 bit), Double (64 bit)
+    val myFloat: Float = 13.37F
+    val myDouble: Double = 3.14159265358979323846
 
-    //Assignment Operators (+=, -+, /=, *=)
+    // Booleans the type Boolean is used to represent logical values.
+    // It can have two possible values true and false.
+    var isSunny: Boolean = true
+    // not sunny anymore...
+    isSunny = false
+
+    // Characters
+    val letterChar = 'A'
+    val digitChar = '1'
+
+    // Strings
+    val myStr = "Hello World"
+    var firCharInStr = myStr[0]
+    var lastCharInStr = myStr[myStr.length - 1]
+
+    //Arithmetic operators (+, -, *, /, %)
+    var result = 5+3
+    result = result / 2
+    // alternatively
+    // result /= 2
+    result = result * 5
+    result = result - 1
+    var moduloResult = 5%2
+    println( moduloResult)
+
+    //Comparison operators (==, !=, <, >, <=, >=)
+    val isEqual = 5==3
+    // Concatenation - adding of "Strings"
+    println("isEqual is " + isEqual)
+    val isNotEqual = 5!=5
+    // Kotlin has a feature called String Interpolation.
+    // This feature allows you to directly insert a template expression inside a String.
+    // Template expressions are tiny pieces of code that are evaluated and
+    // their results are concatenated with the original String.
+    // A template expression is prefixed with $ symbol.
+    // Following are examples of String interpolation
+    println("isNotEqual is $isNotEqual")
+
+    println("is5Greater3 ${5 > 3}")
+    println("is5LowerEqual3 ${5 >= 3}")
+    println("is5LowerEqual5 ${5 >= 5}")
+
+    //Assignment operators (+=, -=, *=, /=, %=)
     var myNum = 5
     myNum += 3
-    myNum /= 2
-    myNum *= 3
-    println("My Num $myNum")
+    println("myNum is $myNum")
+    myNum *= 4
+    println("myNum is $myNum")
 
-    //Increment & Decrement Operators (++, --)
+
+    //Increment & Decrement operators (++, --)
     myNum++
-    println("My Num $myNum")
-    println("My Num ${myNum++}")
-    println("My Num ${++myNum}")
-    println("My Num ${--myNum}")
+    println("myNum is $myNum")
+    // increments after use
+    println("myNum is ${myNum++}")
+    // increments before use
+    println("myNum is ${++myNum}")
+    println("myNum is ${--myNum}")
 }
